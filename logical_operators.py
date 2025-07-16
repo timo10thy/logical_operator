b4cb93067043eda5e5c19636b35fa5a08c4a961a
Timothy Ogory <timothyogory@gmail.com>
2025-07-16 13:08:41 +0100

    first commit

---
+++ logical_operators.py
@@ -0,0 +1,9 @@
+#or
+'''
+it evealuate if booth operand are true it 
+evaluate to true if one value is true and 
+the other is false it evaluate to true and 
+if both are false it evealuate to false
+'''
+
+
